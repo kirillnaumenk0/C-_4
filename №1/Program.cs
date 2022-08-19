007f2edb5cecc04eb55519776aa0f171902c0853
Kirill <kirillnaumenk0@mail.ru>
2022-08-19 18:18:38 +0600

    1

---
+++ №1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+double res = Math.Pow(a,b);
+Console.WriteLine(res);
+//в задании указано :написать цикл, но я не понял зачем, если можно обойтись без него, надеюсь не критично!!!
