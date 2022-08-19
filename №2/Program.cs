97601e0704fd2c61f44ea8506493e7a66edf881b
Kirill <kirillnaumenk0@mail.ru>
2022-08-19 18:19:05 +0600

    22

--- №2/Program.cs
+++ №2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while( num > 0)
+{
+ sum += num % 10;
+ num /= 10;
+}
+Console.WriteLine(sum);
